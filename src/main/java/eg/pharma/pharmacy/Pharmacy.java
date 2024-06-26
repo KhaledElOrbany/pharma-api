5c6846801eed9aafa09e6bfa4d5777b5291195d3
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-06-26 23:52:13 +0300

    pharmacy entity

--- src/main/java/eg/pharma/pharmacy/Pharmacy.java
+++ src/main/java/eg/pharma/pharmacy/Pharmacy.java
@@ -1,10 +1,7 @@
 package eg.pharma.pharmacy;
 
 import eg.pharma.audit.Audit;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Pharmacy extends Audit {
@@ -12,4 +9,66 @@ public class Pharmacy extends Audit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String owner;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phone;
+
+    public Pharmacy() {
+    }
+
+    public Pharmacy(String name, String owner, String address, String phone) {
+        this.name = name;
+        this.owner = owner;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
