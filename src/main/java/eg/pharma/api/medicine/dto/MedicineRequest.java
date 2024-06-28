33b5aa72c81129b516b07c060e2b91da4d79287d
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-06-28 22:12:53 +0300

    Medicine dtos

--- src/main/java/eg/pharma/api/medicine/dto/MedicineRequest.java
+++ src/main/java/eg/pharma/api/medicine/dto/MedicineRequest.java
@@ -1,4 +1,41 @@
 package eg.pharma.api.medicine.dto;
 
+import eg.pharma.enums.MedicineType;
+
 public class MedicineRequest {
+    private String name;
+    private String activeIngredient;
+    private String description;
+    private String barcode;
+    private Double price;
+    private MedicineType type;
+    private String notes;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getActiveIngredient() {
+        return activeIngredient;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public MedicineType getType() {
+        return type;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
 }
--- src/main/java/eg/pharma/api/medicine/dto/MedicineResource.java
+++ src/main/java/eg/pharma/api/medicine/dto/MedicineResource.java
@@ -1,6 +1,15 @@
 package eg.pharma.api.medicine.dto;
 
-import org.springframework.stereotype.Service;
+import eg.pharma.enums.MedicineType;
 
-public class MedicineResource {
+public record MedicineResource(
+        Long id,
+        String name,
+        String activeIngredient,
+        String description,
+        String barcode,
+        double price,
+        MedicineType type,
+        String notes
+) {
 }
