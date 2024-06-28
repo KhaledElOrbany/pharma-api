6649d1ab7fa7b98889447f2a00e06be1ac7a8bae
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-06-28 06:02:58 +0300

    User basics

--- src/main/java/eg/pharma/api/user/UserController.java
+++ src/main/java/eg/pharma/api/user/UserController.java
@@ -1,4 +1,15 @@
 package eg.pharma.api.user;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
--- src/main/java/eg/pharma/api/user/UserService.java
+++ src/main/java/eg/pharma/api/user/UserService.java
@@ -1,4 +1,13 @@
 package eg.pharma.api.user;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
