e16a592023b281e59ea0f94aaa127785118ce302
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-22 23:59:46 +0300

    added annotation EnableTransactionManagement

--- src/main/java/eg/pharma/PharmaApplication.java
+++ src/main/java/eg/pharma/PharmaApplication.java
@@ -2,8 +2,10 @@ package eg.pharma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class PharmaApplication {
 
     public static void main(String[] args) {
