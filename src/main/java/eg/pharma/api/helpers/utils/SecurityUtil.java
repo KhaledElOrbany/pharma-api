bf934c4016dd398ea0d65b2fca6db62a68ab7aab
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-26 22:36:49 +0300

    added null check

--- src/main/java/eg/pharma/api/helpers/utils/SecurityUtil.java
+++ src/main/java/eg/pharma/api/helpers/utils/SecurityUtil.java
@@ -6,6 +6,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 public class SecurityUtil {
 
     public static User getCurrentUser() {
+        if (SecurityContextHolder.getContext().getAuthentication() == null) {
+            return null;
+        }
         return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
