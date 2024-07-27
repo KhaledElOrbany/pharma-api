e22d2bb59d0640582f6597080588605c1c10dc2c
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-27 23:06:58 +0300

    added function to generate RefreshToken

--- src/main/java/eg/pharma/api/helpers/services/JwtService.java
+++ src/main/java/eg/pharma/api/helpers/services/JwtService.java
@@ -1,5 +1,6 @@
 package eg.pharma.api.helpers.services;
 
+import eg.pharma.api.features.user.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -86,5 +87,9 @@ public class JwtService {
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateRefreshToken(User user, String token) {
+        return "";
     }
 }
