f37f51b782dcbdeeb4ce080d2a1e372006aedb22
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-27 14:39:03 +0300

    logging some startup info

--- src/main/java/eg/pharma/PharmaApplication.java
+++ src/main/java/eg/pharma/PharmaApplication.java
@@ -1,15 +1,65 @@
 package eg.pharma;
 
+import io.micrometer.common.util.StringUtils;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
 
 @SpringBootApplication
 @EnableTransactionManagement
 public class PharmaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PharmaApplication.class, args);
+    private static final Logger log = LoggerFactory.getLogger(PharmaApplication.class);
+
+    @PostConstruct
+    public void initApplication() {
     }
 
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(PharmaApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store")).map(key -> "https").orElse("http");
+        String applicationName = env.getProperty("spring.application.name");
+        String serverPort = env.getProperty("server.port");
+        String contextPath = Optional.ofNullable(env.getProperty("server.servlet.context-path"))
+                .filter(StringUtils::isNotBlank)
+                .orElse("/");
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("""
+
+                        ----------------------------------------------------------
+                        \tApplication '{}' is running!
+                        \tAccess URLs:
+                        \t* Local\t\t: {}://localhost:{}{}
+                        \t* External\t: {}://{}:{}{}
+                        \t* Profile(s)\t: {}
+                        ----------------------------------------------------------""",
+                applicationName,
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles().length == 0 ? env.getDefaultProfiles() : env.getActiveProfiles()
+        );
+    }
 }
