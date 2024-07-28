27e253481662857bbc8d4daf08c1dd449be65cb9
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-28 10:54:48 +0300

    enhanced SecurityUtil

--- src/main/java/eg/pharma/api/helpers/utils/SecurityUtil.java
+++ src/main/java/eg/pharma/api/helpers/utils/SecurityUtil.java
@@ -1,14 +1,16 @@
 package eg.pharma.api.helpers.utils;
 
 import eg.pharma.api.features.user.User;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityUtil {
 
     public static User getCurrentUser() {
-        if (SecurityContextHolder.getContext().getAuthentication() == null) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated() || "anonymousUser".equals(authentication.getPrincipal())) {
             return null;
         }
-        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return (User) authentication.getPrincipal();
     }
 }
