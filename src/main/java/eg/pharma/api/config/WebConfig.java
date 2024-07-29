0242931a714218de56c19a2a8befeb0cb34e4f93
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-29 23:29:19 +0300

    log requests

--- src/main/java/eg/pharma/api/config/WebConfig.java
+++ src/main/java/eg/pharma/api/config/WebConfig.java
@@ -1,6 +1,7 @@
 package eg.pharma.api.config;
 
 import eg.pharma.api.helpers.interceptors.JwtInterceptor;
+import eg.pharma.api.helpers.interceptors.LoggingInterceptor;
 import eg.pharma.api.helpers.services.JwtService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -18,6 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new JwtInterceptor(jwtService));
+        registry.addInterceptor(new LoggingInterceptor());
     }
 
     @Override
---
+++ src/main/java/eg/pharma/api/helpers/interceptors/LoggingInterceptor.java
@@ -0,0 +1,38 @@
+package eg.pharma.api.helpers.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        request.setAttribute("startTime", System.currentTimeMillis());
+        logger.info("Starting request for URI: {}", request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        long startTime = (Long) request.getAttribute("startTime");
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        logger.info("Completed request for URI: {}. Duration: {} ms", request.getRequestURI(), duration);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        if (ex != null) {
+            logger.error("Request for URI: {} raised an exception: ", request.getRequestURI(), ex);
+        }
+    }
+}
+
